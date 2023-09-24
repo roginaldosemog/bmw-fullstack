87666c683e0aa0df98c0f5f24cf8c2f6fb50254a
Igor Gomes <roginaldosemog@gmail.com>
2023-09-24 10:56:40 -0300

    style: parse files with new prettier config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin')
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
@@ -13,18 +13,18 @@ export default {
     plugin(function ({ addBase, theme }) {
       addBase({
         h1: {
-          fontSize: theme("fontSize.3xl"),
-          fontWeight: theme("fontWeight.extrabold"),
+          fontSize: theme('fontSize.3xl'),
+          fontWeight: theme('fontWeight.extrabold'),
         },
         h2: {
-          fontSize: theme("fontSize.2xl"),
-          fontWeight: theme("fontWeight.extrabold"),
+          fontSize: theme('fontSize.2xl'),
+          fontWeight: theme('fontWeight.extrabold'),
         },
         h3: {
-          fontSize: theme("fontSize.xl"),
-          fontWeight: theme("fontWeight.extrabold"),
+          fontSize: theme('fontSize.xl'),
+          fontWeight: theme('fontWeight.extrabold'),
         },
-      });
+      })
     }),
   ],
-};
+}
