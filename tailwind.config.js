032ec63ad38ab264303098bebb5e793e7b854dc8
Igor Gomes <roginaldosemog@gmail.com>
2023-09-24 10:54:38 -0300

    feat: add heading base styles

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const plugin = require("tailwindcss/plugin");
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: {
+          fontSize: theme("fontSize.3xl"),
+          fontWeight: theme("fontWeight.extrabold"),
+        },
+        h2: {
+          fontSize: theme("fontSize.2xl"),
+          fontWeight: theme("fontWeight.extrabold"),
+        },
+        h3: {
+          fontSize: theme("fontSize.xl"),
+          fontWeight: theme("fontWeight.extrabold"),
+        },
+      });
+    }),
+  ],
+};
