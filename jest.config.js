ddd80cee6e4b15a359e283a137a3a2cbb0c2320e
Igor Gomes <roginaldosemog@gmail.com>
2023-09-25 16:53:25 -0300

    chore: configure jest to collect complete coverage

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,15 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**.config.**',
+    '!**/.**/**',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/scripts/**',
+    '!**/types/**',
+  ],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
