86f819ac0a230797ebf51966fb74faf491a0b563
Igor Gomes <roginaldosemog@gmail.com>
2023-09-25 22:09:54 -0300

    fix: fix type error caused by NextResponse

--- app/api/customers/route.js
+++ app/api/customers/route.js
@@ -7,13 +7,13 @@ export async function POST(request) {
   const { name, email } = await request.json()
   await connectMongoDB()
   await Customer.create({ name, email })
-  return NextResponse.status(201).json({ message: 'Customer created' })
+  return NextResponse.json({ message: 'Customer created' }, { status: 201 })
 }
 
 export async function GET() {
   await connectMongoDB()
   const customers = await Customer.find()
-  return NextResponse.status(200).json({ customers })
+  return NextResponse.json({ customers }, { status: 200 })
 }
 
 export async function DELETE(request) {
@@ -24,14 +24,24 @@ export async function DELETE(request) {
     const deletedCustomer = await Customer.findByIdAndDelete(id)
 
     if (!deletedCustomer) {
-      return NextResponse.status(404).json({ message: 'Customer not found' })
+      return NextResponse.json(
+        {
+          message: 'Customer not found',
+        },
+        { status: 404 },
+      )
     }
 
     await Order.deleteMany({ customer: id })
 
-    return NextResponse.status(204).send()
+    return NextResponse.json({ message: 'Order deleted' }, { status: 200 })
   } catch (error) {
     console.error(error)
-    return NextResponse.status(500).json({ message: 'Internal server error' })
+    return NextResponse.json(
+      {
+        message: 'Internal server error',
+      },
+      { status: 500 },
+    )
   }
 }
